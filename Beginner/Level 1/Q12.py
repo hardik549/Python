6a545175f04d799f9c4d49be81b6694dba7ef7f7
hardik vagrecha <hardikvagrecha@gmail.com>
2024-09-04 09:25:55 -0700

    Q12.py file added

---
+++ Beginner/Level 1/Q12.py
@@ -0,0 +1,11 @@
+def reverse_num(num):
+    rev = 0
+    
+    while num > 0:
+        rem = num % 10 
+        rev = (rev * 10) + rem
+        num //= 10  
+    return rev
+
+num=int(input("Enter the number : "))
+print("Reversed number : ", reverse_num(num))
